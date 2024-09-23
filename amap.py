960135341da7915b1db17a2180cabdd12653d34e
handsome <2512940883@qq.com>
2024-09-23 10:40:06 +0800

    根据查询到数据的地址补充镇一级的数据

--- amap.py
+++ amap.py
@@ -1,99 +1,117 @@
+import json
+
 import requests
 
-AMAP_KEY = "a5d9c797e4599abbbacc692fd9abac50"
 
+class AmapApi:
+    def __init__(self):
+        self.AMAP_KEY = "a5d9c797e4599abbbacc692fd9abac50"
 
-def amap_api(address, city):
+    def amap_api_geo(self, address, city):
+        """
+        高德api地理解析
+        :param address:
+        :param city:
+        :return:
+        """
+        url = f"https://restapi.amap.com/v3/geocode/geo?address={address}&key={self.AMAP_KEY}&city={city}"
+        response = requests.request("GET", url)
+
+        print(response.text)
+
+    # TODO
+    # 把有数据结果的入表，包括level、location 等信息，
+
+    # 有数据的结果
     """
-    高德api地理解析
-    :param address:
-    :param city:
-    :return:
+    {
+        "status": "1",
+        "info": "OK",
+        "infocode": "10000",
+        "count": "1",
+        "geocodes": [
+            {
+                "formatted_address": "湖北省黄冈市麻城市朱金冲",
+                "country": "中国",
+                "province": "湖北省",
+                "citycode": "0713",
+                "city": "黄冈市",
+                "district": "麻城市",
+                "township": [],
+                "neighborhood": {
+                    "name": [],
+                    "type": []
+                },
+                "building": {
+                    "name": [],
+                    "type": []
+                },
+                "adcode": "421181",
+                "street": [],
+                "number": [],
+                "location": "115.153719,31.597735",
+                "level": "村庄"
+            }
+        ]
+    }
+    {
+        "status": "1",
+        "info": "OK",
+        "infocode": "10000",
+        "count": "1",
+        "geocodes": [
+            {
+                "formatted_address": "湖北省黄冈市麻城市富家岗",
+                "country": "中国",
+                "province": "湖北省",
+                "citycode": "0713",
+                "city": "黄冈市",
+                "district": "麻城市",
+                "township": [],
+                "neighborhood": {
+                    "name": [],
+                    "type": []
+                },
+                "building": {
+                    "name": [],
+                    "type": []
+                },
+                "adcode": "421181",
+                "street": [],
+                "number": [],
+                "location": "115.012164,31.017582",
+                "level": "村庄"
+            }
+        ]
+    }
     """
-    url = f"https://restapi.amap.com/v3/geocode/geo?address={address}&key={AMAP_KEY}&city={city}"
-    response = requests.request("GET", url)
+    # 没有数据的结果
+    """
+    {
+        "status": "0",
+        "info": "ENGINE_RESPONSE_DATA_ERROR",
+        "infocode": "30001"
+    }
+    """
 
-    print(response.text)
+    def amap_api_re_geo(self, location, poi_type="", radius=1000, extensions='all', road_level=0):
+        """
+        高德api地理逆解析
+        :param location:
+        :param poi_type:
+        :param radius:
+        :param extensions:
+        :param road_level:
+        :return:
+        """
+        url = f"https://restapi.amap.com/v3/geocode/regeo?key={self.AMAP_KEY}&location={location}&poitype={poi_type}&" \
+              f"radius={radius}&extensions={extensions}&roadlevel={road_level}"
+        response = requests.request("GET", url)
 
+        json_str = response.text
+        data = json.loads(json_str)
 
+        # 读取township的值
+        township = data['regeocode']['addressComponent']['township']
 
-# TODO
-# 把有数据结果的入表，包括level、location 等信息，
-
-
-# 有数据的结果
-"""
-{
-    "status": "1",
-    "info": "OK",
-    "infocode": "10000",
-    "count": "1",
-    "geocodes": [
-        {
-            "formatted_address": "湖北省黄冈市麻城市朱金冲",
-            "country": "中国",
-            "province": "湖北省",
-            "citycode": "0713",
-            "city": "黄冈市",
-            "district": "麻城市",
-            "township": [],
-            "neighborhood": {
-                "name": [],
-                "type": []
-            },
-            "building": {
-                "name": [],
-                "type": []
-            },
-            "adcode": "421181",
-            "street": [],
-            "number": [],
-            "location": "115.153719,31.597735",
-            "level": "村庄"
-        }
-    ]
-}
-{
-    "status": "1",
-    "info": "OK",
-    "infocode": "10000",
-    "count": "1",
-    "geocodes": [
-        {
-            "formatted_address": "湖北省黄冈市麻城市富家岗",
-            "country": "中国",
-            "province": "湖北省",
-            "citycode": "0713",
-            "city": "黄冈市",
-            "district": "麻城市",
-            "township": [],
-            "neighborhood": {
-                "name": [],
-                "type": []
-            },
-            "building": {
-                "name": [],
-                "type": []
-            },
-            "adcode": "421181",
-            "street": [],
-            "number": [],
-            "location": "115.012164,31.017582",
-            "level": "村庄"
-        }
-    ]
-}
-"""
-# 没有数据的结果
-"""
-{
-    "status": "0",
-    "info": "ENGINE_RESPONSE_DATA_ERROR",
-    "infocode": "30001"
-}
-"""
-
-
-
-
-
+        return township
---
+++ db_base.py
@@ -0,0 +1,102 @@
+import pymysql
+
+from amap import AmapApi
+
+"""
+@auther: gql
+
+根据查询到数据的地址补充镇一级的数据
+"""
+
+amap_api = AmapApi()
+
+# 数据库连接配置
+db_config = {
+    'host': '192.168.10.59',  # 例如：'localhost'
+    'user': 'research',  # 您的数据库用户名
+    'password': '6usKz1Pi8aQ7q8WL',  # 您的数据库密码
+    'database': 'address-research-mater',  # 您的数据库名
+    'charset': 'utf8mb4',  # 字符编码
+    'cursorclass': pymysql.cursors.DictCursor  # 使用字典游标
+}
+
+"""
+SELECT * FROM `address-research-mater`.`addresses`;
+"""
+
+"""
+UPDATE `address-research-mater`.`addresses` SET `township` = NULL WHERE `id` = 1;
+"""
+
+
+def se_db():
+    # 连接到数据库
+    try:
+        connection = pymysql.connect(**db_config)
+        print("成功连接到数据库")
+
+        # 创建一个cursor对象
+        with connection.cursor() as cursor:
+            # 编写SQL查询语句
+            sql = "SELECT * FROM `address-research-mater`.`addresses`"
+
+            # 执行SQL语句
+            cursor.execute(sql)
+
+            # 获取所有查询结果
+            results = cursor.fetchall()
+
+            # 打印结果
+            for row in results:
+                # print(row)  # 如果你设置了cursorclass为DictCursor，这里可以直接用print(row['id'], row['name'])来访问字段
+                township = amap_api.amap_api_re_geo(row['location'])
+                if township != "":
+                    update_db(township, row['id'])
+
+
+    except pymysql.MySQLError as e:
+        print(f"数据库错误: {e}")
+    except Exception as e:
+        print(f"发生错误: {e}")
+    finally:
+        # 关闭数据库连接
+        if connection:
+            connection.close()
+            print("数据库连接已关闭")
+
+
+def update_db(township, id_):
+    # 连接到数据库
+    try:
+        connection = pymysql.connect(**db_config)
+        print("成功连接到数据库")
+
+        # 创建一个cursor对象
+        with connection.cursor() as cursor:
+            # 编写SQL更新语句
+            # 注意：使用参数化查询来防止SQL注入
+            sql = "UPDATE `address-research-mater`.`addresses` SET `township` = %s WHERE `id` = %s;"
+
+            # 执行SQL语句
+            cursor.execute(sql, (township, id_))
+
+            # 提交事务
+            connection.commit()
+
+            print(f"成功更新了ID为{id_}的记录")
+
+    except pymysql.MySQLError as e:
+        print(f"数据库错误: {e}")
+        # 如果发生错误，可以选择回滚
+        if connection:
+            connection.rollback()
+    except Exception as e:
+        print(f"发生错误: {e}")
+    finally:
+        # 关闭数据库连接
+        if connection:
+            connection.close()
+            print("数据库连接已关闭")
+
+
+se_db()
